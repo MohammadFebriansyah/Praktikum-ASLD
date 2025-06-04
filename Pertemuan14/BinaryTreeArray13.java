dad44335a8cd9dd247ca18c57fb9c5b7c4c2fd3b
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-06-04 15:41:33 +0700

    Tugas 4

--- Pertemuan14/BinaryTreeArray13.java
+++ Pertemuan14/BinaryTreeArray13.java
@@ -6,6 +6,7 @@ public class BinaryTreeArray13 {
 
     public BinaryTreeArray13() {
         this.dataMahasiswa = new Mahasiswa13[10];
+        this.idxLast = -1;
     }
 
     void populateData(Mahasiswa13 dataMhs[], int idxLast) {
@@ -20,6 +21,27 @@ public class BinaryTreeArray13 {
                 dataMahasiswa[idxStart].tampilInformasi();
                 traverseInOrder(2 * idxStart + 2);
             }
+        }
+    }
+
+    public void add(Mahasiswa13 data) {
+        if (idxLast + 1 >= dataMahasiswa.length) {
+            System.out.println("Array penuh, tidak bisa menambah data.");
+            return;
+        }
+        idxLast++;
+        dataMahasiswa[idxLast] = data;
+    }
+
+    public void traversePreOrder() {
+        traversePreOrderHelper(0);
+    }
+
+    public void traversePreOrderHelper(int idx) {
+        if (idx <= idxLast && dataMahasiswa[idx] != null) {
+            dataMahasiswa[idx].tampilInformasi();
+            traversePreOrderHelper(2 * idx + 1);
+            traversePreOrderHelper(2 * idx + 2);
         }
     }
 }
